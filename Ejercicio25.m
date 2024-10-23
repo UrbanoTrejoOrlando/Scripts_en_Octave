e4bf51964615f2a321c83a25707da3ceba6728cc
WolverinLando <orlandourbanotrejo@gmail.com>
2024-10-22 20:07:57 -0600

    Script para graficar obtener dominio y rango de funciones

---
+++ Ejercicio25.m
@@ -0,0 +1,19 @@
+% Octave Script
+% Title		   :Funci�n Real de Variable Real (EjercicioNumero2)
+% Correo       :orlandourbanotrejo@gmail.com
+% Description  :Script para graficar obtener dominio y rango de funciones
+% Author	   :Orlando Urbano Trejo (Lando)
+% Date		   :27_10_2021
+% Version	   :1
+% Usage		   :octave>path/EjercicioNumero1
+%		       :Requiere aplicaci�n octave, usar linea de comandos
+%		       :https://octaveintro.readthedocs.io/en/latest/index.html
+
+clear
+% Dominio de la Funci�n
+x=-10:2:10
+% Regla de Correspondencia  (funci�n)
+fx= 1+x.^2
+% Plotear funci�n
+plot(x, fx)
+
