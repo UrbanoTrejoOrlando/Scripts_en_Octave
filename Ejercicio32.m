3c7513b7954ea7d83e483a9617d910332ab9a4a0
WolverinLando <orlandourbanotrejo@gmail.com>
2024-10-22 20:26:07 -0600

    Cambios

--- Ejercicio32.m
+++ Ejercicio32.m
@@ -1,14 +1,14 @@
 % Octave Script
-% Title		      :Función Real de Variable Real (EjercicioNumero9)
-% Correo        :urbanoorlando79@gmail.com
+% Title		      :Funciï¿½n Real de Variable Real (EjercicioNumero9)
+% Correo          :urbanoorlando79@gmail.com
 % Description	  :Script para graficar obtener dominio y rango de funciones
 % Author	      :Orlando Urbano Trejo (Lando)
 % Date		      :27_10_2021
 % Version	      :1
 % Usage		      :octave>path/EjercicioNumero1
-%		            :Requiere aplicación octave, usar linea de comandos
-%		            :https://octaveintro.readthedocs.io/en/latest/index.html
+%		          :Requiere aplicaciï¿½n octave, usar linea de comandos
+%		          :https://octaveintro.readthedocs.io/en/latest/index.html
 
 clear
-% Dominio de la función
+% Dominio de la funciï¿½n
 ezplot ('(- x + 2) .* (x <-1) + (x) .* (- 1 <= x) .* (x <= 2) + (2) .* (x> 2)' , [- 10, 10])
