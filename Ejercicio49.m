7c5e62f2d0f128be99971965eb6fd9ada5cde6cc
WolverinLando <orlandourbanotrejo@gmail.com>
2024-10-24 13:07:10 -0600

    Grafique mediante la suma de riemman el area bajo la curva

---
+++ Ejercicio49.m
@@ -0,0 +1,58 @@
+% Octave Script
+% Title	        :Sumas de Rieman
+% Correo        :orlandourbanotrejo@gmail.com
+% Description	: Grafique mediante sumas de riemman 
+%                 el area bajo la curva de la funcion f(x)= x^2 -1 en un intervalo cerrado de [1, 4].
+% Author	    :Orlando Urbano Trejo 
+% Date		    :01_05_2021
+% Version	    :1
+% Usage		    :octave>path/EjercicioNumero1
+%		        :Requiere aplicacion octave, usar linea de comandos
+%		        :https://octaveintro.readthedocs.io/en/latest/index.html
+
+%limpia la pantalla y las variables.
+clc, clear
+areatotal=0;
+%define los intervalos y el numero de rectangulos.
+a = 1;
+b = 4;
+n = 10;
+%determina la longitud de la base o el incremento 
+%de x.
+base = (b-a)/n;
+%definicion de la funcion.
+x = a:base:(b-base);
+%y esta representada por la variable altura
+altura=(x.^2)-1;
+%calcula el valor de cada uno de los rectangulos.
+area = base*altura;
+%En teoria el valor de la sumatoria deberia estar
+%reflejada   en  este  acumulador sin embargo  no 
+%entiendo por que :c
+areatotal += area;
+%imprime los valores obtenidos.
+printf('\n area por rectangulo : %d \n', double(areatotal));
+
+
+%estable    la    linea   que   forma  la funcion
+x2 = linspace(1,4,10);
+y2 = (x.^2)-1;
+%inicio del  intervalo, fin del intervalo, numero 
+%de rectangulos.
+xa = linspace(1,4,10);
+yab = (xa.^2)-1;
+
+%dibuja una  grafica de barras una a lado de otra 
+%sin espacios
+g = bar(xa,yab,'histc');
+
+%permite continuar  graficando  despues de que ya 
+%exista un grafica trazada
+hold on
+
+%Grafica la funcion
+plot(x2,y2);
+
+%ajusta la grafica a los datos.
+axis tight
+
